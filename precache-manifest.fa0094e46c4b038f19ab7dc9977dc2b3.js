423106057f32af21d788a287de140acbcc84aa72
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-05-20 22:19:13 +0800

    Updates

---
+++ precache-manifest.fa0094e46c4b038f19ab7dc9977dc2b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3752c1401733e094199d1737e6afc1e",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "74d2015ce095f849f239",
+    "url": "/pokedex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a928562db6fa226a8c35",
+    "url": "/pokedex/static/js/2.ad2517f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.ad2517f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d2015ce095f849f239",
+    "url": "/pokedex/static/js/main.7897e3ea.chunk.js"
+  },
+  {
+    "revision": "3720d53d7f3565cf8d9c",
+    "url": "/pokedex/static/js/runtime-main.115cfb4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex/static/media/logo.5d5d9eef.svg"
+  }
+]);
