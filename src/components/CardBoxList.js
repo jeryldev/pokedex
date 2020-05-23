ce678f6b488b53e273dce1f7edcf9ac9151c0712
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-05-23 11:45:24 +0800

    added fixed scroll and transition

--- src/components/CardBoxList.js
+++ src/components/CardBoxList.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import CardBox from './CardBox';
 import './Components.css';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 const CardBoxList = ({ pokemons }) => {
+  // console.log('pokemonssssss::', pokemons);
   return (
     <div className='grid-wrapper'>
       {pokemons.map((pokemon, index) => (
-        <CardBox
-          className='pokemon-item'
+        <CSSTransitionGroup
           key={index}
-          id={index}
-          name={pokemon.name}
-          source={pokemon.url}
-        />
+          transitionName='example'
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <CardBox
+            className='pokemon-item'
+            key={index}
+            id={index}
+            name={pokemon.name}
+            source={pokemon.url}
+          />
+        </CSSTransitionGroup>
       ))}
     </div>
   );
