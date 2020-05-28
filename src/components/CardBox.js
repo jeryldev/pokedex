85d5ad74b4c10124de975a32da7b6021f97053b3
Jeryl Donato Estopace <jeryldev@gmail.com>
2020-05-28 21:18:25 +0800

    minor update on the animation of CardBox component

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -19,41 +19,47 @@ const CardBox = ({ source }) => {
   if (isLoading === false) {
     try {
       cardBoxItem = (
-        <Card className='flex-child'>
-          {data.sprites.front_default ? (
-            <Card.Img
-              variant='top'
-              src={data.sprites.front_default}
-              alt={data.name.charAt(0).toUpperCase() + data.name.slice(1)}
-              style={{
-                width: '96px',
-                height: '96px',
-                marginTop: '1.25rem',
-                transition: 'all 300ms ease-in-out 300ms',
-              }}
-            />
-          ) : (
-            <Card.Img
-              variant='top'
-              src={process.env.PUBLIC_URL + '/logo512.png'}
-              alt={data.name.charAt(0).toUpperCase() + data.name.slice(1)}
-              style={{
-                width: '96px',
-                height: '96px',
-                marginTop: '1.25rem',
-                transition: 'all 300ms ease-in-out 300ms',
-              }}
-            />
-          )}
-          <Card.Body>
-            <Card.Title>
-              {data.name.charAt(0).toUpperCase() + data.name.slice(1)}
-            </Card.Title>
-          </Card.Body>
-        </Card>
+        <animated.div style={customStyle}>
+          <Card className='flex-child'>
+            {data.sprites.front_default ? (
+              <Card.Img
+                variant='top'
+                src={data.sprites.front_default}
+                alt={data.name.charAt(0).toUpperCase() + data.name.slice(1)}
+                style={{
+                  width: '96px',
+                  height: '96px',
+                  marginTop: '1.25rem',
+                  transition: 'all 300ms ease-in-out 300ms',
+                }}
+              />
+            ) : (
+              <Card.Img
+                variant='top'
+                src={process.env.PUBLIC_URL + '/logo512.png'}
+                alt={data.name.charAt(0).toUpperCase() + data.name.slice(1)}
+                style={{
+                  width: '96px',
+                  height: '96px',
+                  marginTop: '1.25rem',
+                  transition: 'all 300ms ease-in-out 300ms',
+                }}
+              />
+            )}
+            <Card.Body>
+              <Card.Title>
+                {data.name.charAt(0).toUpperCase() + data.name.slice(1)}
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </animated.div>
       );
     } catch (error) {
-      cardBoxItem = <NoPokemonCardBox message={data} />;
+      cardBoxItem = (
+        <animated.div style={customStyle}>
+          <NoPokemonCardBox message={data} />
+        </animated.div>
+      );
     }
   } else {
     cardBoxItem = <SearchingPokemondCardBox />;
@@ -68,7 +74,7 @@ const CardBox = ({ source }) => {
         transform: `${hovered ? 'scale(1.1)' : 'scale(1)'}`,
       }}
     >
-      <animated.div style={customStyle}>{cardBoxItem}</animated.div>
+      {cardBoxItem}
     </div>
   );
 };
